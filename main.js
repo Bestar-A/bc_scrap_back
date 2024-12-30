d70592d2194116431af87e88aa871c231c2dc511
Muhammad A <muhammada7268@gmail.com>
2024-12-30 16:28:21 +0500

    Add BCGame Backend

---
+++ main.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const { scrapeCrashGameData, scrapeBackground } = require("./scrap");
+const crypto = require("crypto");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const _salt = "0000000000000000000301e2801a9a9598bfb114e574a91a887f2132f33047e6";
+let _hash = null;
+
+function sha256Hash(data) {
+	return crypto.createHash("sha256").update(data).digest("hex");
+}
+
+function gameResult(seed, salt) {
+	const n_bits = 52;
+	const seedBytes = Buffer.from(seed, "hex");
+	const saltBytes = Buffer.from(salt, "utf-8");
+
+	const hmac = crypto.createHmac("sha256", saltBytes);
+	hmac.update(seedBytes);
+	const hmacHex = hmac.digest("hex");
+
+	seed = hmacHex.slice(0, n_bits / 4);
+	const r = parseInt(seed, 16);
+
+	let X = r / 2 ** n_bits;
+	X = Math.round(X * 1e9) / 1e9;
+
+	X = 99 / (1 - X);
+
+	const result = Math.floor(X);
+	return Math.max(1, result / 100);
+}
+
+function backgroundScraping() {
+	setInterval(async () => {
+		let data = await scrapeCrashGameData();
+		if (data === null) return;
+
+		const temp = data;
+		_hash = data;
+		const results = [];
+
+		for (let i = 0; i < 10; i++) {
+			const temp1 = gameResult(temp, _salt);
+			results.push(temp1);
+			temp = sha256Hash(temp);
+		}
+
+		io.emit("update_data", { data: results });
+	}, 1000);
+}
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+	console.log("A user connected");
+
+	socket.on("request_info", () => {
+		socket.emit("send_info", { info: "Data" });
+	});
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+	});
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+backgroundScraping();
+scrapeBackground();
---
+++ scrap.js
@@ -0,0 +1,65 @@
+const puppeteer = require("puppeteer");
+
+var firstGameHash = null;
+
+const scrapeCrashGameData = () => {
+	return firstGameHash;
+};
+
+const scrapeBackground = async () => {
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ["--disable-blink-features=AutomationControlled", "--disable-popup-blocking", "--no-sandbox", "--disable-dev-shm-usage", "--disable-extensions"],
+	});
+
+	const page = await browser.newPage();
+	const url = "https://bc.game/game/crash";
+	await page.goto(url);
+
+	const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+	await sleep(5000);
+
+	const postDataToAPI = async (dataToPost) => {
+		return await page.evaluate(async (data) => {
+			return await fetch("https://bc.game/api/game/bet/multi/history", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			}).then((response) => response.json());
+		}, dataToPost);
+	};
+
+	while (true) {
+		await sleep(3000); // Sleep for 3 seconds
+		const dataToPost = {
+			gameUrl: "crash",
+			page: 1,
+			pageSize: 50,
+		};
+
+		try {
+			const response = await postDataToAPI(dataToPost);
+
+			if (response && typeof response === "object") {
+				const firstGameDetail = response.data?.list[0]?.gameDetail || {};
+				if (Object.keys(firstGameDetail).length > 0) {
+					const gameHash = JSON.parse(firstGameDetail);
+					firstGameHash = gameHash.hash || {};
+				} else {
+					console.log("First game hash not found in the response.");
+				}
+			} else {
+				console.log("Invalid response format.");
+			}
+		} catch (error) {
+			console.log(`Failed to fetch data: ${error}`);
+		}
+	}
+};
+
+module.exports = { scrapeCrashGameData, scrapeBackground };
+
+//scrapeBackground();
