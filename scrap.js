333286459e19a74380e073069085056193abac67
Muhammad A <muhammada7268@gmail.com>
2024-12-30 17:37:30 +0500

    Modify Chrome Version

--- scrap.js
+++ scrap.js
@@ -17,7 +17,7 @@ const scrapeBackground = async () => {
 	});
 
 	const page = await browser.newPage();
-	await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
+	await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.204 Safari/537.36");
 
 	const url = "https://bc.game/game/crash";
 	await page.goto(url);
